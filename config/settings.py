1d5f13ba74f8d6b83468c592e250330b35d4d056
Tiago Ferreira <usaferreiratiago@gmail.com>
2026-07-18 14:53:16 -0300

    qa

--- config/settings.py
+++ config/settings.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     AZURE_OPENAI_ENDPOINT: str
     AZURE_OPENAI_API_VERSION: str = "2024-02-15-preview"
     DEPLOYMENT_NAME: str
+    GOOGLE_API_KEY: str  # Add this line
     
     model_config = SettingsConfigDict(env_file=".env")
 
